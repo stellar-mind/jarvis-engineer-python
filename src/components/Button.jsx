b3dccae4c6144d0458e21f1c6d3bcc6aff5c3e2c
= <=>
2025-03-31 23:37:44 -0300

    feat: adiciona componente e seus testes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const Button = ({ text, color, onPress }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.button, { backgroundColor: color }]}>
-    <Text style={styles.text}>{text}</Text>
-  </TouchableOpacity>
-);
+const Button = ({ text, color, onPress }) => {
+  return (
+    <TouchableOpacity style={[styles.button, {backgroundColor: color}]} onPress={onPress}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   button: {
-    padding: 10,
-    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10,
+    borderRadius: 5,
   },
   text: {
     color: '#fff',
+    fontSize: 16,
   },
 });
 
--- src/components/__tests__/Button.test.jsx
+++ src/components/__tests__/Button.test.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
-import Button from '../Button'; // Caminho para o arquivo Button
+import Button from './Button'; // Path to your Button component
 
-describe('Button', () => {
+describe('<Button />', () => {
   it('renders correctly', () => {
-    const { getByText } = render(<Button title="Click me" onPress={() => {}} />);
-    getByText('Click me'); // Verificando se o texto do botão está correto
+    const { getByText } = render(<Button />);
+    getByText('Button');
   });
 
-  it('executes onPress function on press', () => {
-    const onPressMock = jest.fn(); // Criando um mock para a função onPress
-    const { getByText } = render(<Button title="Click me" onPress={onPressMock} />);
-
-    fireEvent.press(getByText('Click me')); // Simulando o pressionamento do botão
+  it('executes onPress function when clicked', () => {
+    const onPress = jest.fn();
+    const { getByText } = render(<Button onPress={onPress} />);
     
-    expect(onPressMock).toHaveBeenCalled(); // Verificando se a função onPress foi chamada
+    fireEvent.press(getByText('Button'));
+    
+    expect(onPress).toHaveBeenCalled();
   });
 
   it('applies correct backgroundColor', () => {
-    const { getByTestId } = render(<Button title="Click me" onPress={() => {}} color="#000000" />); // Garanta que você tem essa prop na sua componente
-
-    expect(getByTestId('button')).toHaveStyle({ backgroundColor: '#000000' }); // Verificando se a cor do botão está correta
+    const { getByTestId } = render(<Button />);
+    const button = getByTestId('button');
+    
+    expect(button.props.style.backgroundColor).toBe('blue');
   });
 });
